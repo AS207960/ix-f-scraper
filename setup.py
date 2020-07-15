c685b4281902f275d836855cba2d3d498d0bc437
Alfie Foster <bonzi@lordbonzi.pro>
2020-07-15 15:31:24 +0100

    Bump to v0.0.1 to fix networktoolkit dep

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 'networktoolkit']
 
 setup_requirements = [ ]
 
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/as207960/ix-f-scraper',
-    version='0.0.0',
+    version='0.0.1',
     zip_safe=False,
 )
